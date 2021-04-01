cc3a2c5396c442843a91912bc31f06c371d31f7d
jiahaohuang <jiahaohuang@emotibot.com>
2021-04-01 15:34:25 +0800

    update .gitignore

--- code/modules/ClassifierBlock.py
+++ code/modules/ClassifierBlock.py
@@ -6,19 +6,21 @@ import torch.nn as nn
 class ClassifierBlock(nn.Module):
     def __init__(self, cfg):
         super(ClassifierBlock, self).__init__()
+
+        self.use_sum = cfg.use_sum
         self.use_dist = cfg.use_dist
         self.use_cos = cfg.use_cos
 
-        fc_in = cfg.classifier_in
-        if self.use_dist:
+        fc_in = 0
+        if cfg.use_sum:
+            fc_in += cfg.classifier_in
+        if cfg.use_dist:
             fc_in += 1
-        if self.use_cos:
+        if cfg.use_cos:
             fc_in += 1
 
         self.fc_classify = nn.Sequential(
-            nn.Linear(fc_in, cfg.cls_hid), 
-            nn.ReLU(),
-            nn.Linear(cfg.cls_hid, 2)
+            nn.Linear(fc_in, 2)
         )
     
     def inner_product(self, y1, y2):
@@ -28,14 +30,19 @@ class ClassifierBlock(nn.Module):
         return res.view(-1, 1)
     
     def forward(self, vec1, vec2):
-        vec = vec1 + vec2
+        cls_input = []
+        if self.use_sum:
+            vec_sum = torch.cat((vec, vec1 + vec2), dim=1)
+            cls_input.append(vec_sum)
         if self.use_dist:
             delta = vec1 - vec2
-            dist = torch.sqrt(self.inner_product(delta, delta))
-            vec = torch.cat((vec, dist), dim=1)
+            vec_dist = torch.sqrt(self.inner_product(delta, delta))
+            cls_input.append(vec_dist)
         if self.use_cos:
-            cos = self.inner_product(vec1, vec2) / torch.sqrt(self.inner_product(vec1, vec1)) / torch.sqrt(self.inner_product(vec2, vec2))
-            vec = torch.cat((vec, cos), dim=1)
+            vec_cos = self.inner_product(vec1, vec2) / torch.sqrt(self.inner_product(vec1, vec1)) / torch.sqrt(self.inner_product(vec2, vec2))
+            cls_input.append(vec_cos)
+        
+        vec = torch.cat(cls_input, dim=1)
         output = self.fc_classify(vec)
 
         return output
